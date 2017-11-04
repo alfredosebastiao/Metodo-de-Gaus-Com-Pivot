f77cca09aefb0d1dee1f18a96e8f71126cd09c46
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-11-04 18:45:07 +0200

    Adicao dos pacotes do mvc

---
+++ src/visao/teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visao;
+
+/**
+ *
+ * @author User
+ */
+public class teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

bfbae0f72dc8eafe6562acabcbb5ffeca3d3ad4c
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-11-04 19:34:18 +0200

    Adicao de metodo para ir zerar as colunas

--- src/controlo/Controle.java
+++ src/controlo/Controle.java
@@ -14,8 +14,43 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public class Controle {
-    ArrayList<ArrayList<Double>> matriz = new ArrayList();
-    ArrayList<ArrayList<Double>> matrizEscalonada = new ArrayList();
+    ArrayList<ArrayList<Double>> matriz;
+    ArrayList<ArrayList<Double>> matrizEscalonada;
+    int numeroDeVariaveis = 3;
+    
+    public void receberMatriz(ArrayList<ArrayList<Double>> matriz){
+        this.matriz = new ArrayList<>();
+        this.matriz = matriz;
+        this.numeroDeVariaveis = this.matriz.size();
+        
+        
+    }
+    
+    public ArrayList<ArrayList<Double>> retornarMatrizComPivot(ArrayList<ArrayList<Double>> matriz,int coluna){
+        int linhaDoPivot = this.retornarLinhaDoPivot(matriz, coluna);
+        
+        ArrayList<Double> auxParaTroca = new ArrayList();
+        auxParaTroca = matriz.get(coluna);
+        matriz.set(coluna, matriz.get(linhaDoPivot));
+        this.matriz = retornarZeroAbaixo(matriz, coluna);
+        return null;
+    }
     
     
+    public int retornarLinhaDoPivot(ArrayList<ArrayList<Double>> matriz,int coluna){
+        int linha = 0;
+        double maior = 0;
+        for(int i = 0; i < this.numeroDeVariaveis; i++){
+            if(Math.abs(matriz.get(i).get(coluna)) > maior){
+                maior = matriz.get(i).get(coluna);
+                linha = i;
+            }
+        }
+        
+        return linha;
+    }
+    
+    public ArrayList<ArrayList<Double>> retornarZeroAbaixo(ArrayList<ArrayList<Double>> matriz,int coluna){
+        return null;
+    }
 }

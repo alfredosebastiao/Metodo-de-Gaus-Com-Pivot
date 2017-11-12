865b7ca24b9501082bedb0fb03a3d163cd59a119
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-11-12 16:18:33 +0200

    Adicao do painel

--- src/visao/Tela.java
+++ src/visao/Tela.java
@@ -27,33 +27,64 @@ public class Tela extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnPrincipal = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Resolucao de Sistemas de equacao pelo Metodo de Gaus Com pivot");
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Equacao"));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1008, Short.MAX_VALUE)
+            .addGap(0, 480, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 557, Short.MAX_VALUE)
+            .addGap(0, 555, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout pnPrincipalLayout = new javax.swing.GroupLayout(pnPrincipal);
+        pnPrincipal.setLayout(pnPrincipalLayout);
+        pnPrincipalLayout.setHorizontalGroup(
+            pnPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnPrincipalLayout.createSequentialGroup()
+                .addContainerGap(197, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(149, 149, 149))
+            .addGroup(pnPrincipalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnPrincipalLayout.setVerticalGroup(
+            pnPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnPrincipalLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -92,6 +123,8 @@ public class Tela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel pnPrincipal;
     // End of variables declaration//GEN-END:variables
 }
--- src/visao/Teste.java
+++ src/visao/Teste.java
@@ -15,37 +15,37 @@ public class Teste {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       Controle c = new Controle();
-       ArrayList<ArrayList<Double>> matriz = new ArrayList();
-        
-       ArrayList<Double> a = new ArrayList();
-       
-       a.add(1.0);
-       a.add(-1.0);
-       a.add(2.0);
-       a.add(2.0);
-       matriz.add(a);
-       a = new ArrayList();
-       a.add(2.0);
-       a.add(2.0);
-       a.add(-1.0);
-       a.add(0.0);
-       matriz.add(a);
-       a = new ArrayList();
-       a.add(-2.0);
-       a.add(-5.0);
-       a.add(3.0);
-       a.add(3.0);
-       
-       matriz.add(a);
-          
-        ArrayList<Double> results = new ArrayList();
-       results.add(0.0);
-       results.add(2.0);
-       results.add(3.0);
-       c.receberMatriz(matriz, results);
-      
-    }
-    
+//    public static void main(String[] args) {
+//       Controle c = new Controle();
+//       ArrayList<ArrayList<Double>> matriz = new ArrayList();
+//        
+//       ArrayList<Double> a = new ArrayList();
+//       
+//       a.add(1.0);
+//       a.add(-1.0);
+//       a.add(2.0);
+//       a.add(2.0);
+//       matriz.add(a);
+//       a = new ArrayList();
+//       a.add(2.0);
+//       a.add(2.0);
+//       a.add(-1.0);
+//       a.add(0.0);
+//       matriz.add(a);
+//       a = new ArrayList();
+//       a.add(-2.0);
+//       a.add(-5.0);
+//       a.add(3.0);
+//       a.add(3.0);
+//       
+//       matriz.add(a);
+//          
+//        ArrayList<Double> results = new ArrayList();
+//       results.add(0.0);
+//       results.add(2.0);
+//       results.add(3.0);
+//       c.receberMatriz(matriz, results);
+//      
+//    }
+//    
 }
